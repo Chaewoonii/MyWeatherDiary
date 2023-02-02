843dd1965779161d185f9fef33db9884f16ba6c2
chacunKim <cocodata12@gmail.com>
2023-02-02 15:14:13 +0900

    test: RestController, Encoding password 230202

---
+++ MyWeatherDiary/src/main/java/com/cnu/diary/myweatherdiary/test/PswdEncoder.java
@@ -0,0 +1,45 @@
+package com.cnu.diary.myweatherdiary.test;
+
+import lombok.Getter;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+
+@Getter
+public class PswdEncoder {
+
+    private String pswd;
+    public static final String RANDOM_ALGORITHM = "SHA1PRNG";
+    public static final String DIGEST_ALGORITHM = "SHA-256";
+    public static final String FORMAT = "%064x";
+    public SecureRandom secureRandom;
+    public MessageDigest messageDigest;
+
+
+    public PswdEncoder(String pswd) throws NoSuchAlgorithmException {
+        this.pswd = pswd;
+        this.secureRandom = SecureRandom.getInstance(RANDOM_ALGORITHM);
+        this.messageDigest = MessageDigest.getInstance(DIGEST_ALGORITHM);
+    }
+
+    public byte[] getBytes(){
+        byte[] bytes = new byte[16];
+        this.secureRandom.nextBytes(bytes);
+        return bytes;
+    }
+
+    public String getSalt(byte[] bytes){
+        return new String(Base64.getEncoder().encode(bytes));
+    }
+
+    public String getHex(){
+        String pswdAndsalt = this.pswd + this.getSalt(this.getBytes());
+        this.messageDigest.update(pswdAndsalt.getBytes());
+        return String.format(FORMAT, new BigInteger(1, this.messageDigest.digest()));
+    }
+
+}
--- MyWeatherDiary/src/main/java/com/cnu/diary/myweatherdiary/test/PswdRepository.java
+++ MyWeatherDiary/src/main/java/com/cnu/diary/myweatherdiary/test/PswdRepository.java
@@ -1,9 +1,10 @@
-package com.cnu.diary.myweatherdiary.db;
+package com.cnu.diary.myweatherdiary.test;
 
-import com.cnu.diary.myweatherdiary.vo.PswdEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PswdRepository extends CrudRepository<PswdEntity, Long> {
+    @Override
+    <S extends PswdEntity> S save(S entity);
 }
