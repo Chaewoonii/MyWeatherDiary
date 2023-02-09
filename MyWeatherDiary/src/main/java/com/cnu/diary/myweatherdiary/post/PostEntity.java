835138c46c46015c8d49a3a88f6e2a4a18257c7f
chacunKim <cocodata12@gmail.com>
2023-02-09 11:21:46 +0900

    .

--- MyWeatherDiary/src/main/java/com/cnu/diary/myweatherdiary/post/PostEntity.java
+++ MyWeatherDiary/src/main/java/com/cnu/diary/myweatherdiary/post/PostEntity.java
@@ -13,14 +13,21 @@ import java.sql.Time;
 public class PostEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, unique = true)
     private int id;
     @Column(nullable = false, unique = true, length = 100)
     private int pswd_id;
+    @Column(nullable = false, length = 100)
     private String diary_title;
+    @Column(nullable = false)
     private Date post_date;
+    @Column(nullable = false, length =10)
     private String feelings;
+    @Column(nullable = false)
     private Time post_time;
+    @Column(length = 300)
     private String post_comment;
+    @Column
     private String loc_pic;
 
 
