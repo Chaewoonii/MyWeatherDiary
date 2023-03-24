cb81dfe63f921d3338f991a8cb401a529f74fd1b
DESKTOP-FLPOG5V\강성엽 <piz115@naver.com>
2023-03-25 00:16:42 +0900

    :zap: 인코딩 디코딩 코드 개선

--- MyWeatherDiary/src/main/java/com/cnu/diary/myweatherdiary/daily/content/ContentService.java
+++ MyWeatherDiary/src/main/java/com/cnu/diary/myweatherdiary/daily/content/ContentService.java
@@ -61,19 +61,36 @@ public class ContentService {
 
     @Transactional
     @PostMapping("contents")
-    public byte[] imgToBase64(ContentDto contentDto) { // encoder
-        Encoder encoder = Base64.getEncoder();
-        byte[] encoderByte = encoder.encode(contentDto.getImg().getBytes());
+    public List<byte[]> imgToBase64(List<ContentDto> contentDtos) { // encoder
+        List<byte[]> contentImgToByteList = new ArrayList<>();
+        Iterator<ContentDto> iter = contentDtos.iterator();
 
-        return encoderByte;
+        Encoder encoder = Base64.getEncoder();
+        while(iter.hasNext()) {
+            ContentDto contentDto = iter.next();
+            byte[] contentDtoImgByte = contentDto.getImg().getBytes();
+            byte[] encoding = encoder.encode(contentDtoImgByte);
+
+            contentImgToByteList.add(encoding);
+        }
+
+        return contentImgToByteList;
     }
 
 
     @Transactional
-    public byte[] base64ToImg(byte[] encode) { // decoder
-        Decoder decoder = Base64.getDecoder();
-        byte[] decoderByte = decoder.decode(encode);
+    public List<String> base64ToImg(List<byte[]> contentImgToByteList) { // decoder
+        List<String> contentByteToImgList = new ArrayList<>();
+        Iterator<byte[]> iter = contentImgToByteList.iterator();
 
-        return  decoderByte;
+        Decoder decoder = Base64.getDecoder();
+        while(iter.hasNext()) {
+            byte[] byteImg = iter.next();
+
+            byte[] decoding =  decoder.decode(byteImg);
+            contentByteToImgList.add(new String(decoding));
+        }
+
+        return contentByteToImgList;
     }
 }
