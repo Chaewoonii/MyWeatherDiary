1dad6eb8fd5c039e29d4884dabf732da355e0f89
Chaewoonii <cocodata12@gmail.com>
2023-04-21 13:17:35 +0900

    feat: config CORS

--- MyWeatherDiary/src/main/java/com/cnu/diary/myweatherdiary/configuration/WebMvcConfig.java
+++ MyWeatherDiary/src/main/java/com/cnu/diary/myweatherdiary/configuration/WebMvcConfig.java
@@ -5,17 +5,21 @@ import com.cnu.diary.myweatherdiary.utill.EntityConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("POST", "GET", "PUT", "DELETE");
+                .allowedOrigins("http://localhost:3000", "*", "http://localhost:8080")
+                .allowedMethods("POST", "GET", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3000);
     }
 
     @Override
